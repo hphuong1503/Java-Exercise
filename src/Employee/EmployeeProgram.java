e0765ed4032f2c21943d6b29d6bc7eea9b96f5bb
Binh <s00873@bvisvietnam.com>
2023-10-03 19:21:04 +0700

    code ngay 3/10

--- src/Employee/EmployeeProgram.java
+++ src/Employee/EmployeeProgram.java
@@ -1,13 +1,15 @@
 package Employee;
 
-import java.sql.SQLOutput;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class EmployeeProgram {
 
     public static void main(String[] args) {
-        Employee[] employeeList = new Employee[50];
+        boolean condition = true;
+        int numOfEmployee = 10;
+        Scanner scanner = new Scanner(System.in);
+
+        Employee[] employeeList = new Employee[10];
 
         employeeList[0] = new FullTimeEmployee("Ryan", 8);
         employeeList[1] = new FullTimeEmployee("James", 8);
@@ -20,27 +22,53 @@ public class EmployeeProgram {
         employeeList[8] = new PartTimeEmployee("Timmy", 8, 11);
         employeeList[9] = new PartTimeEmployee("Tyson", 8, 2);
 
-        boolean condition = true;
-        int numOfEmployee = 10;
-        Scanner scanner = new Scanner(System.in);
 
-        while (condition){
+
+        while (condition) {
             System.out.println("1: In ra tat ca Employee");
             System.out.println("2: In ra Employee co luong tren $20");
             System.out.println("3: In ra 5 Employee co luong cao nhat");
             System.out.println("4: Search vi tri ten Employee");
             System.out.println("5: Search ky tu");
             System.out.println("6: Add Employee");
-            switch (scanner.nextInt()){
-                case 1: printAllEmployee(employeeList, numOfEmployee); break;
-                case 2: listEmployeeOverTwenty(employeeList); break;
-                case 3: printFiveHighestSalary(employeeList); break;
-                case 4: findEmployeeByName(employeeList); break;
-                case 5: findEmployeeByLetter(employeeList); break;
-                case 6: addNewEmployee(employeeList, numOfEmployee); numOfEmployee++; break;
+            System.out.println("7: Xoa Employee");
+            switch (scanner.nextInt()) {
+                case 1:
+                    printAllEmployee(employeeList, numOfEmployee);
+                    break;
+                case 2:
+                    listEmployeeOverTwenty(employeeList);
+                    break;
+                case 3:
+                    printFiveHighestSalary(employeeList);
+                    break;
+                case 4:
+                    findEmployeeByName(employeeList);
+                    break;
+                case 5:
+                    findEmployeeByLetter(employeeList);
+                    break;
+                case 6:
+                    if(numOfEmployee==employeeList.length) {
+                        Employee employeeExtended[] = new Employee[2 * employeeList.length];
+                        for (int i = 0; i < employeeList.length; i++) {
+                            employeeExtended[i] = employeeList[i];
+                        }
+                        employeeList = employeeExtended;
+                    }
+                    if (addNewEmployee(employeeList, numOfEmployee)) {
+                        numOfEmployee++;
+
+                    }
+                    break;
+                case 7:
+                    if(deleteEmployeeFromArrayList(employeeList, numOfEmployee)){
+                        numOfEmployee--;
+                    }
+                    break;
             }
             System.out.println("Continue? (y/n)");
-            if (scanner.next().equals("n")){
+            if (scanner.next().equals("n")) {
                 condition = false;
             }
         }
@@ -51,6 +79,7 @@ public class EmployeeProgram {
             System.out.println(employeeList[i].getName() + ":" + " " + employeeList[i].calculateSalary());
         }
     }
+
     static void findEmployeeByName(Employee employeeList[]) {
         String name;
         Scanner scanner = new Scanner(System.in);
@@ -58,14 +87,15 @@ public class EmployeeProgram {
         name = scanner.nextLine();
         for (int i = 0; i < 10; i++) {
             if (employeeList[i].getName().equalsIgnoreCase(name)) {
-                System.out.println("Vi tri cua "+name+" la thu: " + (i + 1));
+                System.out.println("Vi tri cua " + name + " la thu: " + (i + 1));
                 return;
             }
         }
-        System.out.println("Khong co employee ten: "+name);
+        System.out.println("Khong co employee ten: " + name);
 
     }
-    static void printFiveHighestSalary(Employee employeeList[]){
+
+    static void printFiveHighestSalary(Employee employeeList[]) {
         Employee[] arr;
         arr = employeeList.clone();
         Employee highestEmp = null;
@@ -83,7 +113,8 @@ public class EmployeeProgram {
             System.out.println(arr[i].getName());
         }
     }
-    static void listEmployeeOverTwenty(Employee employeeList[]){
+
+    static void listEmployeeOverTwenty(Employee employeeList[]) {
         System.out.println("List nguoi co luong cao hon $20");
         for (int i = 0; i < 10; i++) {
             if (employeeList[i].calculateSalary() > 20) {
@@ -100,14 +131,14 @@ public class EmployeeProgram {
         int num = 0;
         for (int i = 0; i < 10; i++) {
             if (employeeList[i].getName().contains(name)) {
-                System.out.println(employeeList[i].getName()+": "+employeeList[i].calculateSalary());
+                System.out.println(employeeList[i].getName() + ": " + employeeList[i].calculateSalary());
                 num++;
             }
         }
-        System.out.println("Co "+num+" nguoi duoc tim thay");
+        System.out.println("Co " + num + " nguoi duoc tim thay");
     }
 
-    static void addNewEmployee(Employee employeeList[], int numOfEmployee){
+    static boolean addNewEmployee(Employee employeeList[], int numOfEmployee) {
         Scanner scanner = new Scanner(System.in);
         String name;
         Employee employee;
@@ -115,16 +146,18 @@ public class EmployeeProgram {
         int workingHours;
         System.out.println("1: Add full time employee");
         System.out.println("2: Add part time employee");
-        switch (scanner.nextInt()){
-            case 1: System.out.println("Nhap ten Employee: ");
+        switch (scanner.nextInt()) {
+            case 1:
+                System.out.println("Nhap ten Employee: ");
                 name = scanner.next();
                 System.out.println("Nhap luong mot gio: ");
                 pay = scanner.nextInt();
                 employee = new FullTimeEmployee(name, pay);
                 employeeList[numOfEmployee] = employee;
-                break;
+                return true;
 
-            case 2: System.out.println("Nhap ten Employee: ");
+            case 2:
+                System.out.println("Nhap ten Employee: ");
                 name = scanner.next();
                 System.out.println("Nhap luong mot gio: ");
                 pay = scanner.nextInt();
@@ -132,12 +165,31 @@ public class EmployeeProgram {
                 workingHours = scanner.nextInt();
                 employee = new PartTimeEmployee(name, pay, workingHours);
                 employeeList[numOfEmployee] = employee;
-                break;
+                return true;
         }
+        return false;
+    }
+
+    public static boolean deleteEmployeeFromArrayList(Employee[] employeeList, int numOfEmployee) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Nhap ten nhan vien muon xoa");
+        String name = scan.nextLine();
+        //Tim vi tri cua nhan vien
+        int position = -1;
+        for (int j = 0; j < numOfEmployee; j++) {
+            if (employeeList[j].getName().equalsIgnoreCase(name)) {
+                position = j;
+            }
+        }
+
+        if (position > -1) {
+            for (int i = position; i < numOfEmployee - 1; i++) {
+                employeeList[i] = employeeList[i + 1];
+            }
+            return true;
+        }
+        return false;
+    }
+
+
 }
-
-
-
-}
-
-
